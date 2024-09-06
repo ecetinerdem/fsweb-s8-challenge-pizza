4e7f833ffb1a22555487d2f2db2c35eca10f0d16
Cetin Erdem <cetoerdem1@gmail.com>
2024-09-06 15:04:17 +0400

    relative vh, rem, em düzenlendi. Toast çalışıyor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import OrderForm from './components/OrderForm';
+import { toast, ToastContainer, Bounce } from 'react-toastify'; 
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
@@ -13,6 +15,19 @@ function App() {
         <Route exact path="/" component={MainPage} />
         <Route path="/orderform" component={OrderForm} />
       </Switch>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
     </Router>
   );
 }
--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,5 +1,35 @@
 export default  function OrderForm() {
     return (
-        <h1>Order</h1>
+        <>
+            <div>
+                <h1>Teknolojik Yemekler</h1>
+                <a href="">Anasayfa</a>
+                <a href="">Seçenekler</a>
+                <a href="">Sipraiş oluştur</a>
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+            <div>
+                <h3>Sipariş notu</h3>
+                <input type="text" />
+            </div>
+            <div>
+                <div>counter</div>
+                <div>card</div>
+            </div>
+        </>
     )
 }
